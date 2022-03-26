ec4f6fcb6c1c318a0b2766efce82bfa1f67ac5d9
sunchenguang <sunchenguang5@huawei.com>
2022-03-26 11:42:56 +0800

    modified test_xdevice
    Signed-off-by: sunchenguang <sunchenguang5@huawei.com>

--- extension/src/xdevice_extension/_core/driver/parser.py
+++ extension/src/xdevice_extension/_core/driver/parser.py
@@ -34,7 +34,7 @@ from xdevice import ResultCode
 from xdevice_extension._core.constants import CommonParserType
 
 __all__ = ["CppTestParser", "CppTestListParser",
-           "JunitParser", "JSUnitParser"]
+           "JunitParser", "JSUnitParser", "OHKernelTestParser"]
 
 _INFORMATIONAL_MARKER = "[----------]"
 _START_TEST_RUN_MARKER = "[==========] Running"
@@ -54,6 +54,16 @@ _END_JSUNIT_RUN_MARKER = "[end] run suites end"
 _PASS_JSUNIT_MARKER = "[pass]"
 _FAIL_JSUNIT_MARKER = "[fail]"
 _ACE_LOG_MARKER = "jsapp"
+
+"""
+OpenHarmony Kernel Test
+"""
+RUNTEST_TEST = "runtest test"
+START_TO_TEST = "Start to test"
+FINISHED_TO_TEST = "Finished to test"
+TIMEOUT_TESTCASES = "Timeout testcases"
+FAIL_DOT = "FAIL."
+PASS_DOT = "PASS."
 
 LOG = platform_logger("Parser")
 
@@ -774,3 +784,111 @@ class JSUnitParser(IParser):
             ''.join((self.state_machine.test().stacktrace, message))
 
 
+@Plugin(type=Plugin.PARSER, id=CommonParserType.oh_kernel_test)
+class OHKernelTestParser(IParser):
+
+    def __init__(self):
+        self.state_machine = StateRecorder()
+        self.suites_name = ""
+        self.listeners = []
+
+    def get_listeners(self):
+        return self.listeners
+
+    def __process__(self, lines):
+        if not self.state_machine.suites_is_started():
+            self.state_machine.trace_logs.extend(lines)
+        for line in lines:
+            self.parse(line)
+
+    def __done__(self):
+        pass
+
+    def parse(self, line):
+        line = re.sub('\x1b.*?m', '', line)
+        if self.state_machine.suites_is_started() or RUNTEST_TEST in line:
+            if RUNTEST_TEST in line:
+                self.handle_suites_started_tag(line)
+            elif START_TO_TEST in line:
+                self.handle_suite_start_tag(line)
+            elif FINISHED_TO_TEST in line:
+                self.handle_suite_end_tag(line)
+            elif line.endswith(PASS_DOT) or line.endswith(FAIL_DOT):
+                self.handle_one_test_case_tag(line)
+            elif TIMEOUT_TESTCASES in line:
+                self.handle_suites_ended_tag(line)
+
+    def handle_suites_started_tag(self, line):
+        self.state_machine.get_suites(reset=True)
+        test_suites = self.state_machine.get_suites()
+        test_suites.suites_name = self.suites_name
+        test_suites.test_num = 0
+        for listener in self.get_listeners():
+            suite_report = copy.copy(test_suites)
+            listener.__started__(LifeCycle.TestSuites, suite_report)
+
+    def handle_suites_ended_tag(self, line):
+        suites = self.state_machine.get_suites()
+        suites.is_completed = True
+
+        for listener in self.get_listeners():
+            listener.__ended__(LifeCycle.TestSuites, test_result=suites,
+                               suites_name=suites.suites_name)
+
+    def handle_suite_start_tag(self, line):
+        pattern = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}" \
+                  " Start to test (.+)$"
+        matcher = re.match(pattern, line)
+        if matcher and matcher.group(1):
+            self.state_machine.suite(reset=True)
+            test_suite = self.state_machine.suite()
+            test_suite.suite_name = matcher.group(1)
+            for listener in self.get_listeners():
+                suite_report = copy.copy(test_suite)
+                listener.__started__(LifeCycle.TestSuite, suite_report)
+
+    def handle_suite_end_tag(self, line):
+        pattern = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}" \
+                  " Finished to test (.+)$"
+        matcher = re.match(pattern, line)
+        if matcher and matcher.group(1):
+            suite_result = self.state_machine.suite()
+            suites = self.state_machine.get_suites()
+            suite_result.run_time = suite_result.run_time
+            suites.run_time += suite_result.run_time
+            suite_result.is_completed = True
+
+            for listener in self.get_listeners():
+                suite = copy.copy(suite_result)
+                listener.__ended__(LifeCycle.TestSuite, suite, is_clear=True)
+
+    def handle_one_test_case_tag(self, line):
+        pattern = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} (.+) " \
+                  "(PASS|FAIL)\\.$"
+        matcher = re.match(pattern, line)
+        if not (matcher and matcher.group(1) and matcher.group(2)):
+            return
+        test_result = self.state_machine.test(reset=True)
+        test_suite = self.state_machine.suite()
+        test_result.test_class = test_suite.suite_name
+        test_result.test_name = matcher.group(1)
+        test_result.current = self.state_machine.running_test_index + 1
+        for listener in self.get_listeners():
+            test_result = copy.copy(test_result)
+            listener.__started__(LifeCycle.TestCase, test_result)
+
+        test_suites = self.state_machine.get_suites()
+        if PASS_DOT in line:
+            test_result.code = ResultCode.PASSED.value
+        elif FAIL_DOT in line:
+            test_result.code = ResultCode.FAILED.value
+            for listener in self.get_listeners():
+                result = copy.copy(test_result)
+                listener.__failed__(LifeCycle.TestCase, result)
+        self.state_machine.test().is_completed = True
+        test_suite.test_num += 1
+        test_suites.test_num += 1
+        for listener in self.get_listeners():
+            result = copy.copy(test_result)
+            listener.__ended__(LifeCycle.TestCase, result)
+        self.state_machine.running_test_index += 1
